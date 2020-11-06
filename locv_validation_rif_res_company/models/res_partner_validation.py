e999ca6eeee5419cb5519889654d6f1f7812b9cd
luisris45 <luisris45@gmail.com>
2020-11-06 12:42:52 -0400

    Validaciones

--- locv_validation_rif_res_company/models/res_partner_validation.py
+++ locv_validation_rif_res_company/models/res_partner_validation.py
@@ -6,6 +6,7 @@
 
 from odoo import fields, models, api,exceptions
 import re
+from openerp.exceptions import ValidationError
 
 class RespartnerRif(models.Model):
     _inherit = 'res.partner'
@@ -30,8 +31,111 @@ class RespartnerRif(models.Model):
         res = super(RespartnerRif, self).write(vals)
         return res
 
+    @api.constrains('function', 'x_studio_departamento', 'phone', 'mobile', 'x_studio_telfono_2', 'x_studio_movil_2', 'website', 'x_studio_correo_secundario')
+    def _check_something(self):
+        flag = 0
+        for record in self:
+            mensaje = "Error en los Campos:"
 
+            if record.x_studio_departamento != False:
+                regex = '^[a-zA-Z\s]*$'
+                if (re.search(regex, str(record.x_studio_departamento))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Departamento solo puede contener letras"
+                    mensaje = mensaje + "  \n " + app
+            if record.function != False:
+                regex = '^[a-zA-Z\s]*$'
+                if (re.search(regex, str(record.function))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Cargo solo puede contener letras"
+                    mensaje = mensaje + "  \n " + app
+            if record.phone != False:
+                regex = '^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$'
+                if (re.search(regex, str(record.phone))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Formato erroneo de Telefono"
+                    mensaje = mensaje + "  \n " + app
+            if record.mobile != False:
+                regex = '^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$'
+                if (re.search(regex, str(record.mobile))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Formato erroneo de Móvil"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_telfono_2 != False:
+                regex = '^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$'
+                if (re.search(regex, str(record.x_studio_telfono_2))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Formato erroneo de Telefono 2"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_movil_2 != False:
+                regex = '^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$'
+                if (re.search(regex, str(record.x_studio_movil_2))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Formato erroneo de Móvil 2"
+                    mensaje = mensaje + "  \n " + app
+            if record.website != False:
+                regex = '((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*'
+                if (re.search(regex, str(record.website))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Formato erroneo de Sitio Web"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_correo_secundario != False:
+                regex = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+                if (re.search(regex, str(record.x_studio_correo_secundario))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Formato erroneo de Correo Secundario"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_ranking_en_el_sector != False:
+                regex = '^[0-9#]+$'
+                if (re.search(regex, str(record.x_studio_ranking_en_el_sector))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Ranking del secto solo puede contener números"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_nmero_de_empleados != False:
+                regex = '^[0-9]+$'
+                if (re.search(regex, str(record.x_studio_nmero_de_empleados))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Número de empleados solo puede contener números"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_nmero_de_sedes_yo_sucursales != False:
+                regex = '^[0-9]+$'
+                if (re.search(regex, str(record.x_studio_nmero_de_sedes_yo_sucursales))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Número de sedes solo puede contener números"
+                    mensaje = mensaje + "  \n " + app
+            if record.x_studio_aos_en_el_mercado != False:
+                regex = '^[0-9]+$'
+                if (re.search(regex, str(record.x_studio_aos_en_el_mercado))):
+                    aja = 1
+                else:
+                    flag = 1
+                    app = "Años en el mercado solo puede contener números"
+                    mensaje = mensaje + "  \n " + app
 
+            if flag == 1:
+                raise ValidationError(mensaje)
     @api.model
     def create(self, vals):
         res = {}
