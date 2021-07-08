eb5ce210f307ee9a112ff5456e7e316fb5dc5fd7
angelicastro <angelicastro1997@gmail.com>
2021-07-07 20:41:23 -0400

    Extension de la vista formulario, kanban y tree de crm para esconder los botones de crear y editar opportunidad y propuesta a dos grupos de usuarios respectivamente

---
+++ hide_buttons_opportunity/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hide_create_and_edit_button",
+
+    'summary': """Hide creat and edit button in crm opportunity""",
+
+    'author': "IT Sales",
+
+    'depends': ['base','crm', 'sale'],
+
+    'data': [
+        'security/security.xml',
+        'views/views.xml',
+        
+    ],
+}
